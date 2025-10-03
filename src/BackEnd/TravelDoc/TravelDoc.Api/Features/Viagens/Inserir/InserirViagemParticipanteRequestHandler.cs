09263cc611ac72996ec48ebb99459be4df3baea7
Kelvin Henrique do Nascimento Santos <contato.kelvin@hotmail.com>
2025-10-03 18:58:04 -0300

    Feat: Implementacoes

--- src/BackEnd/TravelDoc/TravelDoc.Api/Features/Viagens/Inserir/InserirViagemParticipanteRequestHandler.cs
+++ src/BackEnd/TravelDoc/TravelDoc.Api/Features/Viagens/Inserir/InserirViagemParticipanteRequestHandler.cs
@@ -3,6 +3,7 @@ using MediatR;
 using TravelDoc.Api.Domain.Viagens.Entities;
 using TravelDoc.Api.Domain.Viagens.Repositories;
 using TravelDoc.Api.Extensions;
+using TravelDoc.Application.Domain.Usuarios.Repositories;
 using TravelDoc.Infrastructure.Core.Results;
 using TravelDoc.Infrastructure.Persistence.Context;
 
@@ -26,24 +27,33 @@ namespace TravelDoc.Api.Features.Viagens.Participantes.Inserir
     public record InserirViagemParticipanteRequest : IRequest<Result>
     {
         public required int ViagemId { get; set; }
-        public required int ParticipanteId { get; set; }
-        public required StatusViagemParticipanteEnum Status { get; set; }
+        public required string EmailConvidado { get; set; }
+        public  StatusViagemParticipanteEnum? Status { get; set; }
     }
 
     public class InserirViagemParticipanteRequestHandler : IRequestHandler<InserirViagemParticipanteRequest, Result>
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IViagemParticipanteRepository _viagemParticipanteRepository;
+        private readonly IUsuarioRepository _usuarioRepository;
 
-        public InserirViagemParticipanteRequestHandler(IUnitOfWork unitOfWork, IViagemParticipanteRepository viagemParticipanteRepository)
+        public InserirViagemParticipanteRequestHandler(IUnitOfWork unitOfWork, IViagemParticipanteRepository viagemParticipanteRepository, IUsuarioRepository usuarioRepository)
         {
             _unitOfWork = unitOfWork;
             _viagemParticipanteRepository = viagemParticipanteRepository;
+            _usuarioRepository = usuarioRepository;
         }
 
         public async Task<Result> Handle(InserirViagemParticipanteRequest request, CancellationToken cancellationToken)
         {
-            var participanteResult = ViagemParticipante.Criar(request.ViagemId, request.ParticipanteId, request.Status);
+            var usuarioConvidado = await _usuarioRepository.ObterAsync(request.EmailConvidado);
+
+            if (usuarioConvidado == null)
+            {
+                return Result.Failure("Convidado não possui no cadastro no TravelDoc");
+            }
+         
+            var participanteResult = ViagemParticipante.Criar(request.ViagemId, usuarioConvidado.Id, StatusViagemParticipanteEnum.Pendente);
             if (participanteResult.IsFailure)
             {
                 return Result.Failure(participanteResult.Error);
@@ -64,13 +74,13 @@ namespace TravelDoc.Api.Features.Viagens.Participantes.Inserir
                 .GreaterThan(0)
                 .WithMessage("ViagemId é obrigatório");
 
-            RuleFor(x => x.ParticipanteId)
-                .GreaterThan(0)
-                .WithMessage("ParticipanteId é obrigatório");
+            RuleFor(x => x.EmailConvidado)
+                .NotEmpty()
+                .WithMessage("O e-mail é obrigatório")
+                .MinimumLength(3)
+                .WithMessage("O nome deve ter no mínimo 3 caracteres");
 
-            RuleFor(x => x.Status)
-                .IsInEnum()
-                .WithMessage("Status inválido");
+
         }
     }
 }
